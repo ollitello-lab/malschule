39685d49ad1d01b3900c45a50f4465bffe5aa53f
Olli Thoma <ollitello@googlemail.com>
2025-10-18 16:50:47 +0200

    chore: import CMSimple 5.20 (1:1 under public/)

--- public/2site2lang/index.php
+++
@@ -1,5 +0,0 @@
-<?php /* utf8-marker = äöü */
-$pth['folder']['base'] = '../../';
-
-include($pth['folder']['base'] . 'cmsimple/cms.php');
-?>
--- public/setup/defaults/log.php
+++
@@ -1,3 +0,0 @@
-<?php // utf-8 marker: äöü
-if(!defined('CMSIMPLE_VERSION') || preg_match('/log.php/i', $_SERVER['SCRIPT_NAME'])){die('No direct access');} ?>
-==============================
