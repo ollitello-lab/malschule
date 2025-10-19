8323e10e2ab23c42303decf0d605820546496cb6
Olli Thoma <ollitello@googlemail.com>
2025-10-19 09:26:17 +0200

    cleanup: remove nested public/public repository

---
+++ public/cmsimple/log.php
@@ -0,0 +1,8 @@
+<?php // utf-8 marker: äöü
+if(!defined('CMSIMPLE_VERSION') || preg_match('/log.php/i', $_SERVER['SCRIPT_NAME'])){die('No direct access');} ?>
+==============================
+2025-10-18 16:49:33 from 2a02:8106:242:3700:537:d1f9:833c:294d logged_in: / - ""
+2025-10-18 16:45:48 from 2a02:8106:242:3700:537:d1f9:833c:294d login failed: / ##### "" ##### 
+<?php // utf-8 marker: äöü
+if(!defined('CMSIMPLE_VERSION') || preg_match('/log.php/i', $_SERVER['SCRIPT_NAME'])){die('No direct access');} ?>
+==============================
